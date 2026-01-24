79a4ee90aa046d58c7015723346d40b0e793ef49
Kapildevreddy <gbr.kapildevreddy@gmail.com>
2026-01-24 16:13:18 +0530

    application and predict pipeline

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import os
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+import pickle
 from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
@@ -13,7 +14,7 @@ def save_object(file_path,obj):
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path,exist_ok=True)
         with open(file_path,'wb') as file_obj:
-            dill.dump(obj,file_obj)
+            pickle.dump(obj,file_obj)
     except Exception as e:
         raise CustomException(e,sys)
     
@@ -44,3 +45,12 @@ def evalute_models(X_train,y_train,X_test,y_test,models,param):
     
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
